a401d2d678a9f41a4869166cff4852565fcd8070
Sona Ramachandran <sonaramachandran46@gmail.com>
2026-03-26 10:13:52 +0530

    Add files via upload

--- reverse.sh
+++ reverse.sh
@@ -1,9 +1,23 @@
-read  num
-reverse=$(echo  " $num " | rev)
-if [ "$num" -eq "$reverse" ]
- then
+#!/bin/bash
 
-   echo "$num is same when reversed"
+echo "Enter a number:"
+read num
+
+original=$num
+reverse=0
+
+while [ $num -gt 0 ]
+do
+    digit=$((num % 10))
+    reverse=$((reverse * 10 + digit))
+    num=$((num / 10))
+done
+
+if [ $original -eq $reverse ]
+then
+    echo "The number $original and its reverse $reverse are the same."
+    echo "It is a palindrome."
 else
-   echo "$num is not same when reversed"
+    echo "The number $original and its reverse $reverse are not the same."
+    echo "It is not a palindrome."
 fi
